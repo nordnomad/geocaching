7a1ee95eda521968071b1b4255c5bd7e00f761fb
Yevhen_Ushakov <ushakov.evgeniy2010@gmail.com>
2014-12-10 11:52:49 +0200

    on geocacheactivity added tabs

--- app/src/main/java/geocaching/ui/GeoCacheActivity.java
+++ app/src/main/java/geocaching/ui/GeoCacheActivity.java
@@ -2,18 +2,81 @@ package geocaching.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import geocaching.tasks.LoadInfoTask;
+import geocaching.common.SlidingTabLayout;
 import map.test.myapplication3.app.R;
 
 public class GeoCacheActivity extends Activity {
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_cache);
-        long geoCacheId = getIntent().getLongExtra("geoCacheId", 0);
-        new LoadInfoTask(this).execute(geoCacheId);
+//        long geoCacheId = getIntent().getLongExtra("geoCacheId", 0);
+//        new LoadInfoTask(this).execute(geoCacheId);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new GeoCachePagerAdapter());
+
+        // Give the SlidingTabLayout the ViewPager, this must be
+        // done AFTER the ViewPager has had it's PagerAdapter set.
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        slidingTabLayout.setViewPager(viewPager);
+    }
+
+    class GeoCachePagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Информация";
+                case 1:
+                    return "Коментарии";
+                case 2:
+                    return "Фотографии";
+            }
+            return "Ошибка";
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            // Inflate a new layout from our resources
+            View view = GeoCacheActivity.this.getLayoutInflater().inflate(R.layout.pager_item, container, false);
+            // Add the newly created View to the ViewPager
+            container.addView(view);
+
+            // Retrieve a TextView from the inflated View, and update it's text
+            TextView title = (TextView) view.findViewById(R.id.item_title);
+            title.setText(String.valueOf(position + 1));
+
+            // Return the View
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 
 }
