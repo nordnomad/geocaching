cd5b6b2231e6f2137482129a1742b39ff301a417
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2015-10-22 15:42:11 +0300

    refactoring for new Navigation Drawer

--- app/src/main/java/geocaching/ui/MainActivity.java
+++ app/src/main/java/geocaching/ui/MainActivity.java
@@ -1,92 +1,72 @@
 package geocaching.ui;
 
-import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import map.test.myapplication3.app.R;
-
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     AccountAuthenticatorResponse accountAuthenticatorResponse;
     Bundle resultBundle;
-
-    String[] menuItems;
-    DrawerLayout menuLayout;
-    ListView menuList;
-
-    ActionBarDrawerToggle drawerToggle;
-
+    ActionBar actionBar = getSupportActionBar();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new android.support.v7.app.ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        TextView textView = (TextView) findViewById(R.id.userNameView);
+        textView.setText(getUserName());
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        actionBar = getSupportActionBar();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, new MapScreen()).commit();
+
+            actionBar.setTitle(getResources().getString(R.string.drawer_menu_map));
+        }
+    }
+
+    private String getUserName() {
         accountAuthenticatorResponse = getIntent().getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
         if (accountAuthenticatorResponse != null) {
             accountAuthenticatorResponse.onRequestContinued();
         }
-        Account[] accounts = AccountManager.get(this).getAccountsByType("com.geocaching");
-
-
-        setContentView(R.layout.activity_maps);
-        menuItems = new String[]{accounts[0].name, "Карта", "Избранное"/*, "Настройки"*/};
-        menuLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        menuList = (ListView) findViewById(R.id.left_drawer);
-//        menuList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, menuItems));
-//        class DrawerItemClickListener implements ListView.OnItemClickListener {
-//            @Override
-//            public void onItemClick(AdapterView parent, View view, int position, long id) {
-//                selectItem(position);
-//            }
-//        }
-//        menuList.setOnItemClickListener(new DrawerItemClickListener());
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        drawerToggle = new ActionBarDrawerToggle(this, menuLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-            public void onDrawerClosed(View view) {
-                supportInvalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle("Geocaching");
-                supportInvalidateOptionsMenu();
-            }
-        };
-        menuLayout.setDrawerListener(drawerToggle);
-
-        if (savedInstanceState == null) {
-            selectItem(1);
-        }
+        return AccountManager.get(this).getAccountsByType("com.geocaching")[0].name;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
+//        if (drawerToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
 //        switch (item.getItemId()) {
 //            case R.id.action_search:
 //                menuLayout.isDrawerOpen(item.getActionView());
@@ -99,134 +79,13 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void selectItem(int position) {
-        Fragment fragment = null;
-        switch (position) {
-            case 0:
-                fragment = new ProfileScreen();
-                break;
-            case 1:
-                fragment = new MapScreen();
-                break;
-            case 2:
-                fragment = new FavoritesScreen();
-                break;
-            default:
-                break;
-        }
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame, fragment).commit();
-//            menuList.setItemChecked(position, true);
-//            getSupportActionBar().setTitle(menuItems[position]);
-//            menuLayout.closeDrawer(menuList);
-        } else {
-            Log.e(getLocalClassName(), "Error. Fragment is not created");
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.map_screen_action_bar, menu);
-//        MenuItem item = menu.findItem(R.id.action_search);
-//        SearchView searchView = (SearchView) item.getActionView();
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
-//        searchView.setSearchableInfo(info);
-
-//        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
-//            @Override
-//            public boolean onMenuItemActionCollapse(MenuItem item) {
-//                // Do something when collapsed
-//                return true;  // Return true to collapse action view
-//            }
-//
-//            @Override
-//            public boolean onMenuItemActionExpand(MenuItem item) {
-//                // Do something when expanded
-//                return true;  // Return true to expand action view
-//            }
-//        });
-
-//        return true;
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView = (SearchView) menu.findItem(R.id.map_search).getActionView();
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        getMenuInflater().inflate(R.menu.map_screen_action_bar, menu);
         return true;
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        if (drawerToggle != null) drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-//    public void attemptLogin() {
-//        if (!isOnline()) {
-//            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-//            AlertDialog alertDialog = alertDialogBuilder
-//                    .setTitle("Network is required")
-//                    .setMessage("You are offline, please connect to internet to be able to login!")
-//                    .setCancelable(false)
-//                    .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int id) {
-//                            dialog.cancel();
-//                        }
-//                    })
-//                    .create();
-//            alertDialog.show();
-//            return;
-//        }
-//        if (authTask != null) {
-//            return;
-//        }
-//        emailView.setError(null);
-//        passwordView.setError(null);
-//
-//        String email = emailView.getText().toString();
-//        String password = passwordView.getText().toString();
-//
-//        boolean cancel = false;
-//        View focusView = null;
-//        if (isBlank(password)) {
-//            passwordView.setError(getString(R.string.error_invalid_password));
-//            focusView = passwordView;
-//            cancel = true;
-//        }
-//        if (isBlank(email)) {
-//            emailView.setError(getString(R.string.error_field_required));
-//            focusView = emailView;
-//            cancel = true;
-//        }
-//        if (cancel) {
-//            focusView.requestFocus();
-//        } else {
-//            authTask = new UserLoginTask(this, email, password);
-//            authTask.execute((Void) null);
-//        }
-//    }
-
-    private boolean isOnline() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnected();
-    }
-
-    public final void setAccountAuthenticatorResult(Bundle result) {
-        resultBundle = result;
-    }
-
     public void finish() {
         if (accountAuthenticatorResponse != null) {
             // send the result bundle back if set, otherwise send an error.
@@ -241,26 +100,37 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem menuItem) {
+    public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camara) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
+        int id = item.getItemId();
+        Fragment fragment = null;
+        if (id == R.id.nav_map) {
+            fragment = new MapScreen();
+        } else if (id == R.id.nav_favourite) {
+            fragment = new FavoritesScreen();
+        } else if (id == R.id.nav_profile) {
+            fragment = new ProfileScreen();
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment).commit();
+            actionBar.setTitle(item.getTitle());
+        } else {
+            Log.e(getLocalClassName(), "Error. Fragment is not created");
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
